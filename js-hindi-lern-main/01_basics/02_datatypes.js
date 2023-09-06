c7cfc9120430766dcd16cad993e0ac2744148332
VALANDOYUSH18 <piyushvaland0000@gmail.com>
2023-09-06 18:43:51 +0530

    first commit

---
+++ js-hindi-lern-main/01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict;"// treat all js code as newer version 
+
+// alert(3+3) // we are using nodejs, not browser 
+  console.log(3 
+    +
+    3)//code readability should be high
+
+    console.log("piyush")
+
+    let name ="piyush"
+    let age = 21;
+    let isLoggedIn = false
+    let state;
+
+    
+
+
+    // number = 2 to powar 53
+    // bigint
+    // String => ""
+    //boolean => true/false
+    // null   => standalon value
+    // undefined =>
+    // symbol => unique  
+
+
+    //object 
+    console.log(typeof age);
+    console.log(typeof null);       0//object
+    console.log(typesof undefined; // o/p =>undefined       .
---
+++ js-hindi-lern-main/README.md
@@ -0,0 +1,2 @@
+# js-hindi-lern
+A code rapo  for the javascript learing series at chai with code youtube channal.
